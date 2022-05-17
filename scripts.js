906a538c5c00d52fe06a2294f52dec7097519217
matheusoliveira121 <matheusvbv@hotmail.com>
2022-05-17 11:20:47 -0300

    formulario estilizado

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// let nameUser;
+// let button;
+
+// button = document.getElementById('btnEnviar');
+
+// button.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     nameUser = document.getElementById('nameUser').value;
+
+//     alert(nameUser)
+// });
+
+// $("#btnEnviar").on('click', (event) => {
+//     event.preventDefault();
+
+//     alert($("#nameUser").val());
+// });
